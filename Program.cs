9a5f5ff75fcf01cb265db49710755fc812ef62f9
Ekwav <ekwav@users.noreply.github.com>
2026-06-08 10:30:07 +0200

    build: update packages to 10.0.8 for .NET 10

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Coflnet.Security.OpenBao;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +13,7 @@ namespace Coflnet.Sky.Referral
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((_, config) => config.AddOpenBaoFromEnvironment())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
